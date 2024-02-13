318b861d6ee3a6531cbea35ca38b830e9e392a17
m7madDaraghmeh <daraghmehmohammed07@gmail.com>
2024-02-13 18:05:49 +0300

    implemnt cart and checkout

--- store/admin.py
+++ store/admin.py
@@ -4,5 +4,5 @@ from .models import *
 admin.site.register(Customer)
 admin.site.register(Product)
 admin.site.register(Order)
-admin.site.register(OrderIitem)
+admin.site.register(OrderItem)
 admin.site.register(ShippingAddress)
--- store/views.py
+++ store/views.py
@@ -7,10 +7,25 @@ def store(request):
     context={'products' : products}
     return render(request , 'store/store.html' , context)
 def cart(request):
-    context={}
+
+    if request.user.is_authenticated:
+        customer= request.user.customer
+        order, created=Order.objects.get_or_create(customer=customer , complete=False) #this function query an object if it doesnot created then create it 
+        items = order.orderitem_set.all() #query the child object by setting parent value then the child object so it will give all orderitems that have the order variable as parent 
+    else:
+        items=[]
+        order={ 'get_cart_total' : 0 , 'get_cart_items' : 0 }
+    context={'items' : items , 'order' : order}
     return render(request , 'store/cart.html' , context)
 def checkout(request):
-    context={}
+    if request.user.is_authenticated:
+        customer= request.user.customer
+        order, created=Order.objects.get_or_create(customer=customer , complete=False) #this function query an object if it doesnot created then create it 
+        items = order.orderitem_set.all() #query the child object by setting parent value then the child object so it will give all orderitems that have the order variable as parent 
+    else:
+        items=[]
+        order={ 'get_cart_total' : 0 , 'get_cart_items' : 0 }
+    context={'items' : items , 'order' : order}
     return render(request , 'store/checkout.html' , context)
 
 
