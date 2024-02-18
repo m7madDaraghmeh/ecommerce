a7e8e8233adfead2b8336f44a806f4199fafbac7
m7madDaraghmeh <daraghmehmohammed07@gmail.com>
2024-02-18 17:59:15 +0300

    edit cart js

--- static/js/cart.js
+++ static/js/cart.js
@@ -10,8 +10,36 @@ for (var i = 0; i < updateBtns.length; i++) {
         if (user === "AnonymousUser") {
             console.log('user is not logged in');
         } else {
-            console.log('user is logged in');
+            updateUserOrder(productId, action)
         }
     });
 }
 
+function getCookie(name) {
+    var value = "; " + document.cookie;
+    var parts = value.split("; " + name + "=");
+    if (parts.length == 2) return parts.pop().split(";").shift();
+}
+
+function updateUserOrder(productId, action) {
+    console.log('user is logged in , sending data')
+    var url = '/update_item/';
+
+    fetch(url, {
+        method: 'post',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-CSRFToken': getCookie('csrftoken')
+        },
+        body: JSON.stringify({ 'productId': productId, 'action': action })
+    })
+        .then((response) => {
+            return response.json()
+        })
+        .then((data) => {
+            console.log('data:', data)
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
