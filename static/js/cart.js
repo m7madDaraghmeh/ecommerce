46bbe1fcdee1fcc07276638ee4212f637512073a
m7madDaraghmeh <daraghmehmohammed07@gmail.com>
2024-02-27 01:14:47 +0300

    js is done

--- static/js/cart.js
+++ static/js/cart.js
@@ -8,11 +8,35 @@ for (var i = 0; i < updateBtns.length; i++) {
         console.log('USER:', user);
 
         if (user === "AnonymousUser") {
-            console.log('user is not logged in');
+            addCookie(productId, action)
         } else {
             updateUserOrder(productId, action)
         }
     });
+}
+
+function addCookie(productId, action){ 
+     console.log('not logged in.......')
+     if(action == 'add'){
+        if(cart[productId] == undefined){
+            cart[productId] = {'quantity':1}
+        }
+        else{
+            cart[productId]['quantity'] += 1
+        }
+     }
+     if(action == 'remove'){
+        cart[productId]['quantity'] -= 1
+        if(cart[productId]['quantity'] <= 0){
+            console.log('remove ')
+            delete cart[productId]
+
+        }
+
+     }
+     console.log("cart : " , cart)
+     document.cookie = 'cart=' + JSON.stringify(cart) + ";domain=;path=/";
+     location.reload()
 }
 
 
--- store/views.py
+++ store/views.py
@@ -29,9 +29,18 @@ def cart(request):
         items = order.orderitem_set.all() #query the child object by setting parent value then the child object so it will give all orderitems that have the order variable as parent 
         cartItems= order.get_cart_items
     else:
+        try:
+            cart= json.loads(request.COOKIES['cart'])
+        except:
+            cart = {}  
+        print('Cart:' , cart)
         items=[]
         order={ 'get_cart_total' : 0 , 'get_cart_items' : 0 ,'shipping' : False}
         cartItems=order['get_cart_items']
+
+        for i in cart :
+            cartItems += cart[i]["quantity"]
+
     context={'items' : items , 'order' : order ,'cartItems' : cartItems}
     return render(request , 'store/cart.html' , context)
 def checkout(request):
